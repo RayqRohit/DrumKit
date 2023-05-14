c056680b61a6505a309b3f7586b234709c59abfe
RayqRohit <118603954+RayqRohit@users.noreply.github.com>
2023-05-14 15:14:39 +0530

    New Website for fun
    for Fun

---
+++ index.js
@@ -0,0 +1,91 @@
+
+// Detecting Button Press
+
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleClick); // for 1st button only
+
+    function handleClick() {
+
+
+        var buttonInnerHTML = this.innerHTML;
+
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+        // var audio = new Audio("sounds/tom-3.mp3");
+        // audio.play();
+    }
+}
+
+
+// Detecting Keyboard Press
+
+
+document.addEventListener("keypress", function (event) {
+
+    makeSound(event.key); 
+    buttonAnimation(event.key);  // event ka "key" wala property
+});
+
+
+
+function makeSound(key) {
+    switch (key) {
+
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+
+        case "a":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+
+        case "s":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+
+        case "d":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+
+        case "j":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+
+        case "k":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+
+        case "l":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+
+
+        default:
+            console.log(buttonInnerHTML);
+
+    }
+
+}
+
+
+function buttonAnimation(currentKey){
+
+var buttonPressed = document.querySelector("."+currentKey);
+
+buttonPressed.classList.add("pressed");
+
+setTimeout(function(){
+    buttonPressed.classList.remove("pressed");
+},100);
+
+}
